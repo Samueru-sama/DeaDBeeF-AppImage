b880e2bce115dca8093fd845b7109912a04b698a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-11-13 10:12:49 -0400

    Update deadbeef-appimage.sh

--- deadbeef-appimage.sh
+++ deadbeef-appimage.sh
@@ -17,47 +17,49 @@ UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|continuous|*$A
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 
 # Prepare AppDir
-mkdir -p "$APP"/AppDir/shared/share/applications
+mkdir -p "$APP"/AppDir/usr/share/applications
 cd "$APP"/AppDir
 url="$(wget -q "$SITE" -O - | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep "x86_64.tar.bz2")"
 wget $url -O download.tar.bz2
 tar fx ./*.tar*
 rm -f ./*.tar*
 
-mv ./deadbeef* ./shared/bin
-ln -s ./bin/lib ./shared/lib
-ln -s ./shared ./usr
+mv ./deadbeef* ./usr/bin
+mv ./usr/bin/lib ./usr/lib
 
 wget "$DESKTOP" -O ./"$APP".desktop
 wget "$ICON" -O ./deadbeef.svg
 sed -i 's/DeaDBeeF/DeaDBeeF Nightly/g' ./"$APP".desktop
 cp ./"$APP".desktop ./usr/share/applications
 
-# ADD LIBRARIES
-# not bundling GTK2
-find . -type f -iname '*GTK2*' -delete
-wget "$LIB4BN" -O ./lib4bin
-chmod +x ./lib4bin
-./lib4bin -v -r -w ./shared/bin/"$TARGET_BIN"
+# Deploy all libs
+cp -vn /usr/lib/libgtk-* ./usr/lib
+ldd ./usr/lib/* | awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./usr/lib
+ldd ./usr/bin/deadbeef | awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./usr/lib
 
-ln ./sharun ./AppRun
-find ./shared/lib -type f -name '*.so*' -exec ldd {} \; \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./shared/lib
-./sharun -g
+cp -vn /lib64/ld-linux-x86-64.so.2 ./usr/lib
 
 # DEPLOY GDK
-#echo "Deploying gdk..."
-#GDK_PATH="$(find /usr/lib -type d -regex ".*/gdk-pixbuf-2.0" -print -quit)"
-#cp -rv "$GDK_PATH" ./shared/lib
+echo "Deploying gdk..."
+GDK_PATH="$(find /usr/lib -type d -regex ".*/gdk-pixbuf-2.0" -print -quit)"
+cp -rv "$GDK_PATH" ./usr/lib
 
-#echo "Deploying gdk deps..."
-#find ./shared/lib/gdk-pixbuf-2.0 -type f -name '*.so*' -exec ldd {} \; \
-#	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./shared/lib
-#find ./shared/lib -type f -regex '.*gdk.*loaders.cache' \
-#	-exec sed -i 's|/.*lib.*/gdk-pixbuf.*/.*/loaders/||g' {} \;
+echo "Deploying gdk deps..."
+find ./usr/lib/gdk-pixbuf-2.0 -type f -name '*.so*' -exec ldd {} \; \
+	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./usr/lib
+find ./usr/lib -type f -regex '.*gdk.*loaders.cache' \
+	-exec sed -i 's|/.*lib.*/gdk-pixbuf.*/.*/loaders/||g' {} \;
+
+# Create AppRun
+echo '#!/bin/sh
+CURRENTDIR="$(readlink -f "$(dirname "$0")")"
+exec "$CURRENTDIR"/usr/lib/ld-linux-x86-64.so.2 \
+	--library-path "$CURRENTDIR"/usr/lib \
+	"$CURRENTDIR"/usr/bin/deadbeef "$@"' > ./AppRun
+chmod +x ./AppRun
 
 # Strip everything
-find ./shared -type f -exec strip -s -R .comment --strip-unneeded {} ';'
+find ./usr -type f -exec strip -s -R .comment --strip-unneeded {} ';'
 
 # MAKE APPIAMGE WITH FUSE3 COMPATIBLE APPIMAGETOOL
 cd ..
