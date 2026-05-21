c8de30c66ed277b8f13cc009c1fb2840123517d3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-21 14:51:33 -0400

    refactor

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -51,100 +51,74 @@ mkdir -p "$BUILD_DIR"
 
 # Clone upstream source
 git clone https://github.com/DeaDBeeF-Player/deadbeef.git "$SRC_DIR"
-cd "$SRC_DIR"
 
-if [ "$DEVEL_RELEASE" = true ]; then
-	echo "Building nightly release..."
-	DEADBEEF_VERSION=$(git rev-parse --short HEAD)
-else
-	echo "Building stable release..."
-	git fetch --tags
-	TAG=$(git tag --sort=-v:refname | grep -vi 'rc\|alpha' | grep -v '^v0\.' | head -1)
-	git checkout "$TAG"
-	DEADBEEF_VERSION="$TAG"
-fi
-
-echo "$DEADBEEF_VERSION" > ~/version
-echo "m4_define([DEADBEEF_VERSION], [$DEADBEEF_VERSION])" > ./build_data/version.m4
-
-# Initialize submodules (external plugins)
-git submodule update --init --recursive
-
-# Download static-deps for headers only (we don't link against the .a files)
-STATICDEPS_URL="http://sourceforge.net/projects/deadbeef/files/staticdeps/ddb-static-deps-latest.tar.bz2/download"
-mkdir -p "$SRC_DIR"/static-deps
-wget "$STATICDEPS_URL" -O "$BUILD_DIR/ddb-static-deps.tar.bz2"
-tar jxf "$BUILD_DIR"/ddb-static-deps.tar.bz2 -C "$SRC_DIR"/static-deps
-
-# Remove static libs from static-deps (incompatible glibc symbol versions)
-find "$SRC_DIR/static-deps" -name "*.a" -delete
-
-# Build ffmpeg 4.4 statically for proper glibc symbol versioning
-FFMPEG_INSTALL_DIR="$BUILD_DIR/ffmpeg-static"
-if [ ! -d "$FFMPEG_INSTALL_DIR/lib" ]; then
+# Build ffmpeg 4.4 statically (in subshell to preserve CWD)
+FFMPEG_INSTALL_DIR=$BUILD_DIR/ffmpeg-static
+if [ ! -d "$FFMPEG_INSTALL_DIR"/lib ]; then
 	FFMPEG_BUILD_DIR="$BUILD_DIR/ffmpeg-4.4"
-	wget -q https://ffmpeg.org/releases/ffmpeg-4.4.tar.xz -O "$BUILD_DIR/ffmpeg-4.4.tar.xz"
-	tar xf "$BUILD_DIR/ffmpeg-4.4.tar.xz" -C "$BUILD_DIR"
-	cd "$FFMPEG_BUILD_DIR"
-	./configure \
-		--prefix="$FFMPEG_INSTALL_DIR"   \
-		--enable-pic                     \
-		--enable-static                  \
-		--disable-shared                 \
-		--enable-gpl                     \
-		--disable-doc                    \
-		--disable-ffplay                 \
-		--disable-ffprobe                \
-		--disable-avdevice               \
-		--disable-ffmpeg                 \
-		--disable-postproc               \
-		--disable-swresample             \
-		--disable-avfilter               \
-		--disable-swscale                \
-		--disable-swscale-alpha          \
-		--disable-vdpau                  \
-		--disable-videotoolbox           \
-		--disable-dxva2                  \
-		--disable-vaapi                  \
-		--disable-hwaccels               \
-		--disable-encoders               \
-		--disable-muxers                 \
-		--disable-indevs                 \
-		--disable-outdevs                \
-		--disable-devices                \
-		--disable-filters                \
-		--disable-bsfs                   \
-		--disable-bzlib                  \
-		--disable-protocols              \
-		--disable-libopus                \
-		--disable-decoders               \
-		--disable-demuxers               \
-		--disable-parsers                \
-		--enable-decoder=wmapro          \
-		--enable-decoder=wmavoice        \
-		--enable-parser=ac3              \
-		--enable-demuxer=ac3             \
-		--enable-decoder=ac3             \
-		--enable-demuxer=eac3            \
-		--enable-decoder=eac3            \
-		--enable-decoder=amrnb           \
-		--enable-demuxer=asf             \
-		--enable-demuxer=oma             \
-		--enable-demuxer=amr             \
-		--enable-demuxer=tak             \
-		--enable-decoder=tak             \
-		--enable-decoder=dsd_lsbf        \
-		--enable-decoder=dsd_lsbf_planar \
-		--enable-decoder=dsd_msbf        \
-		--enable-decoder=dsd_msbf_planar \
-		--enable-demuxer=dsf             \
-		--enable-demuxer=iff             \
-		--enable-version3                \
-		--disable-asm                    \
-		--enable-protocol=file           \
-		--enable-protocol=http
-	make -j"$(nproc)"
-	make install
+	wget -q https://ffmpeg.org/releases/ffmpeg-4.4.tar.xz -O "$BUILD_DIR"/ffmpeg-4.4.tar.xz
+	tar xf "$BUILD_DIR"/ffmpeg-4.4.tar.xz -C "$BUILD_DIR" && (
+		cd "$FFMPEG_BUILD_DIR"
+		./configure \
+			--prefix="$FFMPEG_INSTALL_DIR"   \
+			--enable-pic                     \
+			--enable-static                  \
+			--disable-shared                 \
+			--enable-gpl                     \
+			--disable-doc                    \
+			--disable-ffplay                 \
+			--disable-ffprobe                \
+			--disable-avdevice               \
+			--disable-ffmpeg                 \
+			--disable-postproc               \
+			--disable-swresample             \
+			--disable-avfilter               \
+			--disable-swscale                \
+			--disable-swscale-alpha          \
+			--disable-vdpau                  \
+			--disable-videotoolbox           \
+			--disable-dxva2                  \
+			--disable-vaapi                  \
+			--disable-hwaccels               \
+			--disable-encoders               \
+			--disable-muxers                 \
+			--disable-indevs                 \
+			--disable-outdevs                \
+			--disable-devices                \
+			--disable-filters                \
+			--disable-bsfs                   \
+			--disable-bzlib                  \
+			--disable-protocols              \
+			--disable-libopus                \
+			--disable-decoders               \
+			--disable-demuxers               \
+			--disable-parsers                \
+			--enable-decoder=wmapro          \
+			--enable-decoder=wmavoice        \
+			--enable-parser=ac3              \
+			--enable-demuxer=ac3             \
+			--enable-decoder=ac3             \
+			--enable-demuxer=eac3            \
+			--enable-decoder=eac3            \
+			--enable-decoder=amrnb           \
+			--enable-demuxer=asf             \
+			--enable-demuxer=oma             \
+			--enable-demuxer=amr             \
+			--enable-demuxer=tak             \
+			--enable-decoder=tak             \
+			--enable-decoder=dsd_lsbf        \
+			--enable-decoder=dsd_lsbf_planar \
+			--enable-decoder=dsd_msbf        \
+			--enable-decoder=dsd_msbf_planar \
+			--enable-demuxer=dsf             \
+			--enable-demuxer=iff             \
+			--enable-version3                \
+			--disable-asm                    \
+			--enable-protocol=file           \
+			--enable-protocol=http
+		make -j"$(nproc)"
+		make install
+	)
 fi
 
 # Map arch names (uname -m format → static-deps directory format)
@@ -154,36 +128,54 @@ case $ARCH in
 	*)       STATIC_DEPS_ARCH="$ARCH"   ;;
 esac
 
-# Set up build environment
+# Set up absolute paths for staging
 STATIC_DEPS=$SRC_DIR/static-deps/lib-$STATIC_DEPS_ARCH
 CONFIGURE_FLAGS=--build=$ARCH-unknown-linux-gnu
-export STATIC_DEPS
-export CFLAGS="\
-	-I$FFMPEG_INSTALL_DIR/include \
-	-I$STATIC_DEPS/include \
-	-Wno-error=incompatible-pointer-types-discards-qualifiers \
-	-Wno-error=implicit-int \
-	-Wno-error=int-conversion"
-export CXXFLAGS="$CFLAGS"
-export LDFLAGS="-L$STATIC_DEPS/lib"
-export PKG_CONFIG_PATH="$FFMPEG_INSTALL_DIR/lib/pkgconfig"
 
-cd "$SRC_DIR"
+# Build deadbeef in a subshell to preserve CWD for AppDir staging
+(
+	cd "$SRC_DIR"
+	if [ "$DEVEL_RELEASE" = true ]; then
+		echo "Building nightly release..."
+		DEADBEEF_VERSION=$(git rev-parse --short HEAD)
+	else
+		echo "Building stable release..."
+		git fetch --tags
+		TAG=$(git tag --sort=-v:refname | grep -vi 'rc\|alpha' | grep -v '^v0\.' | head -1)
+		git checkout "$TAG"
+		DEADBEEF_VERSION="$TAG"
+	fi
 
-# Generate configure script
-./autogen.sh
+	echo "$DEADBEEF_VERSION" > ~/version
+	echo "m4_define([DEADBEEF_VERSION], [$DEADBEEF_VERSION])" > ./build_data/version.m4
 
-# Configure (ffmpeg statically linked via PKG_CONFIG_PATH, rest dynamic)
-./configure \
-	CC=clang CXX=clang++ \
-	CFLAGS="$CFLAGS"     \
-	CXXFLAGS="$CXXFLAGS" \
-	LDFLAGS="$LDFLAGS"   \
-	$CONFIGURE_FLAGS     \
-	--disable-gtk2       \
-	--prefix=/usr
-make -j"$(nproc)"
-make install DESTDIR="$INSTALL_DIR"
+	# Initialize submodules (external plugins)
+	git submodule update --init --recursive
+
+	# Download static-deps for headers only
+	STATICDEPS_URL="http://sourceforge.net/projects/deadbeef/files/staticdeps/ddb-static-deps-latest.tar.bz2/download"
+	mkdir -p static-deps
+	wget "$STATICDEPS_URL" -O "$BUILD_DIR/ddb-static-deps.tar.bz2"
+	tar jxf "$BUILD_DIR/ddb-static-deps.tar.bz2" -C static-deps
+
+	# Remove static libs from static-deps (incompatible glibc symbol versions)
+	find static-deps -name "*.a" -delete
+
+	export CFLAGS="\
+		-I$FFMPEG_INSTALL_DIR/include \
+		-I$STATIC_DEPS/include \
+		-Wno-error=incompatible-pointer-types-discards-qualifiers \
+		-Wno-error=implicit-int \
+		-Wno-error=int-conversion"
+	export CXXFLAGS="$CFLAGS"
+	export LDFLAGS="-L$STATIC_DEPS/lib"
+	export PKG_CONFIG_PATH="$FFMPEG_INSTALL_DIR/lib/pkgconfig"
+
+	./autogen.sh
+	./configure CC=clang CXX=clang++ $CONFIGURE_FLAGS --disable-gtk2 --prefix=/usr
+	make -j"$(nproc)"
+	make install DESTDIR="$INSTALL_DIR"
+)
 
 # Move into AppDir structure
 mkdir -p ./AppDir/bin/plugins
@@ -194,8 +186,7 @@ cp -v  "$STATIC_DEPS"/lib/libBlocksRuntime.so* ./AppDir/bin/
 cp -v  "$STATIC_DEPS"/lib/libdispatch.so*      ./AppDir/bin/
 cp -v  "$STATIC_DEPS"/lib/libcurl.so*          ./AppDir/bin/
 cp -v  "$STATIC_DEPS"/lib/libmbed*             ./AppDir/bin/
-
-chmod +x ./AppDir/bin/deadbeef
-
 # Portable mode marker (triggers /proc/self/exe plugin detection)
 cp -v "$INSTALL_DIR"/usr/share/icons/hicolor/48x48/apps/deadbeef.png ./AppDir/bin/
+
+chmod +x ./AppDir/bin/deadbeef
